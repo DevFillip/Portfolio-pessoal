547737965d0458fe4196c27005ae774df2475faf
Fillip Hudson <Filliphudson222@gmail.com>
2022-07-21 00:27:17 -0300

    navbar finalizada

--- src/layout/navbar/navbar.js
+++ src/layout/navbar/navbar.js
@@ -1,17 +1,33 @@
+import './styles.css'
+import {AiOutlineMenu} from 'react-icons/ai'
+import {GrClose} from 'react-icons/gr'
+import {useState} from 'react'
 
+function Navbar(){ 
+    
+    const [navAtiva, setNavAtiva] = useState(`ul`)
+    const [esconderMenu, setEsconderMenu] = useState(false)
 
-function Navbar(){  
+    const navToggle = () =>{
+    navAtiva === `ul` ? setNavAtiva(`ul ativa`) : setNavAtiva(`ul`)
+
+    setEsconderMenu(!esconderMenu)
+ }
+
     return (
-        <nav>
-            <h1>Fillip</h1>
-            <ul>
-                <li>Home</li>
-                <li>Sobre mim</li>
-                <li>Habilidades</li>
-                <li>Projetos</li>
-                <li>Contato</li>
-
+        <nav className='navbar-container'>
+            <p>Fillip</p>
+            <ul className={navAtiva}>
+                <li className="item">Home</li>
+                <li className="item">Sobre mim</li>
+                <li className="item">Habilidades</li>
+                <li className="item">Projetos</li>
+                <li className="item">Contato</li>
             </ul>
+
+            <div className='toggle'>
+                {!esconderMenu ? <AiOutlineMenu className='menu' onClick={navToggle}/> : <GrClose className='fechar' onClick={navToggle}/>}  
+            </div>
         </nav>
     )
 }
