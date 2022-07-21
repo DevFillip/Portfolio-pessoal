956794d2d7b34c3aba40e3ab51a52ce71b9d6c13
Fillip Hudson <Filliphudson222@gmail.com>
2022-07-20 23:32:50 -0300

    projeto setado, e iniciando a navbar

---
+++ src/layout/navbar/navbar.js
@@ -0,0 +1,19 @@
+
+
+function Navbar(){  
+    return (
+        <nav>
+            <h1>Fillip</h1>
+            <ul>
+                <li>Home</li>
+                <li>Sobre mim</li>
+                <li>Habilidades</li>
+                <li>Projetos</li>
+                <li>Contato</li>
+
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
