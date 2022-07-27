43fae3d11b8e5cb1a1e462d508fea01d4553beea
Fillip Hudson <Filliphudson222@gmail.com>
2022-07-27 19:36:32 -0300

    Adicionei a parte de tecnologias finalmente

--- src/Sessões/Tecnologias/Tecnologias.js
+++ src/Sessões/Tecnologias/Tecnologias.js
@@ -1,54 +1,46 @@
-import './styles.css'
+import './tecnologias.css'
 
 function Tecnologias(){
     return (
- 
+      <div class="habilidades_container">
+            <h1 className='h1-tecnologias'>Tecnologias</h1>
+            <div class="barra">
+                <div class="informacoes">
+                    <span>HTML</span>
+                </div>
+                <div class="barra_progresso html">
+                    <span></span>
+                </div>
+            </div>
 
+            <div class="barra">
+                <div class="informacoes">
+                    <span>CSS</span>
+                </div>
+                <div class="barra_progresso css">
+                    <span></span>
+                </div>
+            </div>
 
-<body>
-  <div class="habilidades_container">
-    <div class="barra">
-      <div class="informacoes">
-        <span>HTML</span>
-      </div>
-      <div class="barra_progresso">
-        <span></span>
-      </div>
-    </div>
-    <div class="barra">
-      <div class="informacoes">
-        <span>CSS</span>
-      </div>
-      <div class="barra_progresso">
-        <span></span>
-      </div>
-    </div>
-    <div class="barra">
-      <div class="informacoes">
-        <span>jQuery</span>
-      </div>
-      <div class="barra_progresso">
-        <span></span>
-      </div>
-    </div>
-    <div class="barra">
-      <div class="informacoes">
-        <span>Python</span>
-      </div>
-      <div class="barra_progresso">
-        <span></span>
-      </div>
-    </div>
-    <div class="barra">
-      <div class="informacoes">
-        <span>MySQL</span>
-      </div>
-      <div class="barra_progresso">
-        <span></span>
-      </div>
-    </div>
-  </div>
-</body>
+            <div class="barra">
+                <div class="informacoes ">
+                    <span>JavaScript</span>
+                </div>
+                <div class="barra_progresso javascript">
+                    <span></span>
+                </div>
+            </div>
+
+    
+            <div class="barra">
+                <div class="informacoes">
+                    <span>React JS</span>
+                </div>
+                <div class="barra_progresso react-js">
+                    <span></span>
+                </div>
+            </div>
+        </div>
     )
 }
 
