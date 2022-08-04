448a26404731bdc45d48a04bba5a314df55433fb
Fillip Hudson <Filliphudson222@gmail.com>
2022-08-04 18:12:52 -0300

    arrumei o contato

--- src/Sessões/Contato/Contato.js
+++ src/Sessões/Contato/Contato.js
@@ -7,36 +7,42 @@ function Contato(){
         <div className="contato-container">
             <p className='p-contato'>Contato</p>
 
-            <div className="formas-contato">
-                <div className="telefone">
-                    <FiPhoneCall />
-                    <div className="dados-telefone"> 
-                        <p className='titulo'>Me ligue</p>
-                        <p>13 98122-7376</p>
+                <div className="formas-contato">
+                    <div className="telefone">
+                        <FiPhoneCall />
+                        <div className="dados-telefone"> 
+                            <p className='titulo'>Me ligue</p>
+                            <p>13 98122-7376</p>
+                        </div>
+                    </div>
+
+                    <div className="email">
+                        <MdOutlineMarkEmailRead />
+                        <div className="dados-email">
+                            <p className='titulo'>Email</p>
+                            <p>filliphudson222@gmail.com</p>
+                        </div>
                     </div>
                 </div>
 
-                <div className="email">
-                    <MdOutlineMarkEmailRead />
-                    <div className="dados-email">
-                        <p className='titulo'>Email</p>
-                        <p>filliphudson222@gmail.com</p>
-                    </div>
+                <div className="form-contato">
+                <form action="https://formsubmit.co/15f3987a18e5486bdfaeb59d97279f47" method="POST">
+                    <input type='text' 
+                        placeholder="Nome"
+                        className='input-nome' 
+                        required
+                    />
+                    <input type='email' 
+                        placeholder="Email" 
+                        className='input-email' 
+                        required
+                    />
+                    <textarea placeholder="Mensagem" className='textarea-mensagem'
+                    required></textarea>
+                    <button className='btn-contato' type='submit'>Enviar mensagem<i class="uil uil-message"></i></button>
+                    </form>
                 </div>
-            </div>
-
-            <div className="form-contato">
-                <input type='text' 
-                       placeholder="Nome"
-                       className='input-nome' 
-                />
-                <input type='email' 
-                       placeholder="Email" 
-                       className='input-email' 
-                />
-                <textarea placeholder="Mensagem" className='textarea-mensagem'></textarea>
-                <button className='btn-contato'>Enviar mensagem<i class="uil uil-message"></i></button>
-            </div>
+            
         </div>
     )
 }   
